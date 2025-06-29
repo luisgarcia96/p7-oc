a60aa79257cf14373eef92a63b538371b21b3aaa
Luis Garcia <luisarmandogarcia96@gmail.com>
2025-06-29 17:09:22 +0200

    Finish Steps 3 & 4 & 5

---
+++ app/src/main/java/com/openclassrooms/arista/data/dao/UserDtoDao.kt
@@ -0,0 +1,19 @@
+package com.openclassrooms.arista.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDtoDao {
+  @Insert
+  suspend fun insertUser(user : UserDto):Long
+
+  @Query("SELECT * FROM user WHERE id = :id")
+  fun getUserById (id: Long): Flow<UserDto?>
+
+  @Query("DELETE FROM user WHERE id = :id")
+  suspend fun deleteUserById (id:Long)
+}
